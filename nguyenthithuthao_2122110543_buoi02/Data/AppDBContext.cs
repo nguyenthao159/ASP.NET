7418d51deb05b5856ce63eaffc615a1a6a212b29
Nguyen Thi Thu Thao <nguyenthaoken159@gmail.com>
2025-03-28 15:58:22 +0700

    Buoi2

---
+++ nguyenthithuthao_2122110543_buoi02/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nguyenthithuthao_2122110543_buoi02.Model;
+
+namespace nguyenthithuthao_2122110543_buoi02.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ nguyenthithuthao_2122110543_buoi02/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace nguyenthithuthao_2122110543_buoi02.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public Double Price { get; set; }
+    }
+}
